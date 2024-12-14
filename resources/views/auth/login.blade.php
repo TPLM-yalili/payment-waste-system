d69b11918f348261f1beebea6d7bd1b009d2e9c5
azzikra1s <azzikra.ismunandar08@gmail.com>
2024-12-14 08:49:40 +0700

    update `login.blade.php`

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,17 @@
 <x-guest-layout>
-    <x-slot:tag_line>
-        Selamat datang di Kapays!
-    </x-slot:tag_line>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <a href="/auth/google" class="inline-flex  items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-base text-white tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-        <x-google-logo class="mr-2" /> <span>Login dengan Google</span>
-    </a>
+    <form class="card-body">
+        <div class="form-control"> 
+            <h1 class="text-2xl text-center font-medium mb-4">Login</h1>
+            <p class="text-center mb-5 text-sm">
+                Masuk dengan Google.
+            </p>
+            <a href="/auth/google" class="btn btn-outline">
+                <x-google-logo class="mr-2" /> <span>Login dengan Google</span>
+            </a>
+        <!-- <div class="form-control mt-6">
+          <button class="btn btn-neutral">Login</button>
+        </div> -->
+      </form>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,20 +16,21 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased bg-gray-100">
-    <div class="min-h-screen flex flex-col md:justify-center md:items-center mx-10 pt-6">
-        <div>
-            <a href="/">
-                <x-application-logo class="w-16 h-16 rounded-lg fill-current text-gray-500" />
-            </a>
-            <h1 class="text-5xl font-bold pt-4">Kapays</h1>
-            <p class="py-6 text-center mt-5">
-                {{ $tag_line ?? '' }}
+<body class="font-sans text-gray-900 antialiased bg-white">
+<div class="hero min-h-screen">
+    <div class="hero-content flex-col lg:flex-row">
+        <div class="text-center lg:text-left">
+            <x-application-logo class="w-16 h-16 mb-6 rounded-lg fill-current text-gray-500" />
+            <h1 class="text-5xl font-bold">Login ke Kapays!</h1>
+            <p class="py-6">
+                Untuk melanjutkan ke Kapays, silahkan login terlebih dahulu menggunakan akun google Anda.
             </p>
         </div>
-
-        {{$slot}}
+        <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            {{ $slot }}
+        </div>
     </div>
+</div>
 </body>
 
 </html>

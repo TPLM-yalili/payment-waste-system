fb2890d2254662d90ae10e58fbe4a012c4e7c1cb
azzikra1s <azzikra.ismunandar08@gmail.com>
2024-12-19 18:59:37 +0700

    Update fix form

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -21,17 +21,41 @@
             <!-- Username -->
             <div>
                 <x-input-label for="username" :value="__('Username')" />
-                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"
-                    required autofocus autocomplete="username" />
+                <label class="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        class="h-4 w-4 opacity-70">
+                        <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                <input id="username" class="block mt-1 w-full grow" placeholder="Username" type="text" name="username" :value="old('username')"
+                required autofocus autocomplete="username" />
+                </label>
                 <x-input-error :messages="$errors->get('username')" class="mt-2" />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                
+                <label class="input input-bordered flex items-center gap-2">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        class="h-4 w-4 opacity-70">
+                        <path
+                        fill-rule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clip-rule="evenodd" />
+                    </svg>
+                    <input id="password" class="block mt-1 w-full grow" type="password" placeholder="⦁ ⦁ ⦁ ⦁ ⦁ ⦁" name="password" required
                     autocomplete="new-password" />
+                </label>
+                <!-- <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="new-password" /> -->
 
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>

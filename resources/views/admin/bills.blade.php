8a6a23021a4b03083e3e8923fed1b288115ac133
azzikra1s <azzikra.ismunandar08@gmail.com>
2024-12-19 17:58:19 +0700

    Update `bills.blade.php` button style

--- resources/views/admin/bills.blade.php
+++ resources/views/admin/bills.blade.php
@@ -42,7 +42,7 @@
                 <!-- Button to generate invoices -->
                 <form action="{{ route('generate.invoices') }}" method="POST" class="bg-white shadow-md rounded-lg">
                     @csrf
-                    <button type="submit" class="btn btn-primary w-full h-full">
+                    <button type="submit" class="btn bg-gradient-to-r from-blue-600 to-indigo-600 hover:btn-primary  text-base-200 w-full h-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>

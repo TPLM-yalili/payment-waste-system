c62f43d501f4e0a87c7dde71763c1622930b779b
azzikra1s <azzikra.ismunandar08@gmail.com>
2024-12-19 19:05:30 +0700

    Update `admin-login` password placeholder

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -51,7 +51,7 @@
                         d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                         clip-rule="evenodd" />
                     </svg>
-                    <input id="password" class="block mt-1 w-full grow" type="password" placeholder="⦁ ⦁ ⦁ ⦁ ⦁ ⦁" name="password" required
+                    <input id="password" class="block mt-1 w-full grow" type="password" placeholder="⦁ ⦁ ⦁ ⦁ ⦁ ⦁ ⦁" name="password" required
                     autocomplete="new-password" />
                 </label>
                 <!-- <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
